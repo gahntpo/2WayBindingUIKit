a40571658255cf4c92af27dbd6e7f9ca8bcba098
Karin Prater <karin.prater@gmx.de>
2021-03-29 17:24:04 +0300

    Initial Commit

---
+++ DataFlowProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataFlowProject
+//
+//  Created by Karin Prater on 29.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
